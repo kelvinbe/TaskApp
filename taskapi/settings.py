9bd4e067a1a5f61ed031df217dcd7bbe7595d230
Beno <kelvin.beno.k@gmail.com>
2025-08-15 15:42:54 +0300

    allowed hosts update

--- taskapi/settings.py
+++ taskapi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&*f^$gx29mqg(1w6pc#!xm!puaw=+tsdp%zff@q**iw=6ft&u_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -137,5 +137,5 @@ CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
-    "https://taskapp-3.onrender.com/"
+    "https://taskapp-3.onrender.com"
 ]
