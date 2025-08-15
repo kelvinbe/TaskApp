ca19c3dd7562dade93839199cda9c0a55b017542
Beno <kelvin.beno.k@gmail.com>
2025-08-15 14:16:00 +0300

    updates

--- task-manager-frontend/src/api/api.js
+++ task-manager-frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8000/api", // Django backend URL
+  baseURL: "http://localhost:8000/api",
 });
 
 export default API;
